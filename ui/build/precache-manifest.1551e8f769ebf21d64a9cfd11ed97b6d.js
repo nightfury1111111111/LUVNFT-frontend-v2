5ac06d136614d96a0fa09b0d0240f236bc316127
linjay0208 <linjay0208@gmail.com>
2022-02-19 00:20:23 +0800

    build

---
+++ ui/build/precache-manifest.1551e8f769ebf21d64a9cfd11ed97b6d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d3ef009f32f205b829364dd1a36afa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4065f03723587a4bcb6b",
+    "url": "/static/css/2.e53b7123.chunk.css"
+  },
+  {
+    "revision": "c723c65f39f28d912304",
+    "url": "/static/css/main.c7820b95.chunk.css"
+  },
+  {
+    "revision": "4065f03723587a4bcb6b",
+    "url": "/static/js/2.b97284cd.chunk.js"
+  },
+  {
+    "revision": "58cea19167742e1f1a2f649a708670f8",
+    "url": "/static/js/2.b97284cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c723c65f39f28d912304",
+    "url": "/static/js/main.edb92ec6.chunk.js"
+  },
+  {
+    "revision": "07459d3d5b52495bfcf5",
+    "url": "/static/js/runtime-main.717a8c11.js"
+  },
+  {
+    "revision": "d0f8ff97f70473bd80d75126aa3fa766",
+    "url": "/static/media/city.d0f8ff97.svg"
+  },
+  {
+    "revision": "89dfd6ceeb8504e204f439cdfc1cd762",
+    "url": "/static/media/country.89dfd6ce.svg"
+  },
+  {
+    "revision": "d7b3c671f351198f5ad64f791b2cf54c",
+    "url": "/static/media/landmark.d7b3c671.svg"
+  },
+  {
+    "revision": "6c04b244381655de28f327096e5f4352",
+    "url": "/static/media/stadium.6c04b244.png"
+  },
+  {
+    "revision": "55014986b6bfe2e594cdde45103e7738",
+    "url": "/static/media/status.55014986.png"
+  }
+]);
